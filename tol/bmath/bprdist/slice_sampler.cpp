00c2c9f70cc35c2dbb14e5638154eac3855bcd82
Jorge Pérez <josp.jorge@gmail.com>
2008-11-21 13:08:58 +0000

    slice_sampler is now more robust to invalid density functions.
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@206 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/bmath/bprdist/slice_sampler.cpp
+++ tol/bmath/bprdist/slice_sampler.cpp
@@ -55,80 +55,126 @@ static int uni_slice_evals = 0;
   the slice_sampler_1D function going into an infinite loop.
 */
 
-double slice_sampler_1D(const gsl_rng *rng,
-                        /* initial point */
-                        double x0,
-                        /* log of the probability density (plus constant) */
-                        double (*g)(double,void*),
-                        /* client data for g */
-                        void *gdata=NULL,
-                        /* gx0 = g(x0,gdata) */
-                        double *gx0=NULL,
-                        /* step's size for creating interval (default 1) */
-                        double w=1.0,
-                        /* Limit on steps (negative means infinite) */
-                        int m=-1,
-                        /* lower bound on support of the distribution */
-                        double lower=GSL_NEGINF,
+#define _SS_ZERO_ 1.0e-10
+#define _SS_MAXITER_ 5000
+
+double slice_sampler_1D( const gsl_rng *rng,
+                         /* initial point */
+                         double x0,
+                         /* log of the probability density (plus constant) */
+                         double (*g)(double,void*),
+                         /* client data for g */
+                         void *gdata=NULL,
+                         /* gx0 = g(x0,gdata) */
+                         double *gx0=NULL,
+                         /* step's size for creating interval (default 1) */
+                         double w=1.0,
+                         /* Limit on steps (negative means infinite) */
+                         int m=-1,
+                         /* lower bound on support of the distribution */
+                         double lower=GSL_NEGINF,
                          /* upper bound on support of the distribution */   
-                        double upper=GSL_POSINF)
+                         double upper=GSL_POSINF )
 {
-  double _gx0;
+  double _gx0, _gx;
 
   ++uni_slice_calls;
 
   /* Find the log density at the initial point, if not already known. */
 
-  if (!gx0) {
+  if ( !gx0 ) {
     ++uni_slice_evals;
-    _gx0 = (*g)(x0, gdata);
+    _gx0 = ( *g )( x0, gdata );
   } else _gx0 = *gx0;
 
-  /* Determine the slice level, in log terms. */
+  if ( !gsl_finite( _gx0 ) ) {
+    /* we can not start from undefined density value */
+    return x0;
+  }
 
-  double logy = _gx0 - gsl_ran_exponential(rng,1.0);
+  /* Determine the slice level, in log terms. */
+  double logy = _gx0 - gsl_ran_exponential( rng, 1.0 );
 
   /* Find the initial interval to sample from. */
 
-  double u = gsl_rng_uniform(rng)*w;
+  double u = gsl_rng_uniform( rng )*w;
   double L = x0 - u;
-  double R = x0 + (w-u);  /*should guarantee that x0 is in [L,R], even with roundoff */
+  double R = x0 + ( w - u );  /*should guarantee that x0 is in [L,R], even with roundoff */
 
+  if ( L > lower ) {
+    _gx = ( *g )( L, gdata );
+    if ( !gsl_finite( _gx0 ) ) {
+      /* found undefined density value while expanding interval */
+      return x0;
+    }
+  }
+  if ( R < upper ) {
+    _gx = ( *g )( R, gdata );
+    if ( !gsl_finite( _gx0 ) ) {
+      /* found  undefined density value while expanding interval */
+      return x0;
+    }
+  }
+  
   /*
      Expand the interval until its ends are outside the slice, or until
      the limit on steps is reached.
   */
 
-  if (m<0) { /* no limit on number of steps */
-    while(1) {
-      if (L<=lower) break;
+  if ( m < 0 ) { /* no limit on number of steps */
+    while( 1 ) {
+      if ( L <= lower ) break;
       ++uni_slice_evals;
-      if ((*g)(L, gdata) <= logy) break;
+      _gx = ( *g )( L, gdata );
+      if ( !gsl_finite( _gx0 ) ) {
+        /* found  undefined density value while expanding interval */
+        L += w;
+        break;
+      }
+      if ( _gx <= logy ) break;
       L -= w;
     }
 
-    while(1) {
-      if (R>=upper) break;
+    while( 1 ) {
+      if ( R >= upper ) break;
       ++uni_slice_evals;
-      if ((*g)(R, gdata) <= logy) break;
+      _gx = ( *g )( R, gdata );
+      if ( !gsl_finite( _gx0 ) ) {
+        /* found  undefined density value while expanding interval */
+        R -= w;
+        break;
+      }
+      if ( _gx <= logy ) break;
       R += w;
     }
-  } else if (m > 1) { /* limit on steps, bigger than one */
-    size_t J = gsl_rng_uniform_int(rng, m);
-    size_t K = (m-1) - J;
+  } else if ( m > 1 ) { /* limit on steps, bigger than one */
+    size_t J = gsl_rng_uniform_int( rng, m );
+    size_t K = ( m-1 ) - J;
 
-    while (J > 0) {
-      if (L <= lower) break;
+    while ( J > 0 ) {
+      if ( L <= lower ) break;
       ++uni_slice_evals;
-      if ((*g)(L,gdata) <= logy) break;
+      _gx = ( *g )( L, gdata );
+      if ( !gsl_finite( _gx0 ) ) {
+        /* found  undefined density value while expanding interval */
+        L += w;
+        break;
+      }
+      if ( _gx <= logy ) break;
       L -= w;
       --J;
     }
 
-    while (K>0) {
-      if (R >= upper) break;
+    while ( K > 0 ) {
+      if ( R >= upper ) break;
       ++uni_slice_evals;
-      if ((*g)(R,gdata) <= logy) break;
+      _gx = ( *g )( R, gdata );
+      if ( !gsl_finite( _gx0 ) ) {
+        /* found  undefined density value while expanding interval */
+        R -= w;
+        break;
+      }
+      if ( _gx <= logy ) break;
       R += w;
       --K;
     }
@@ -136,32 +182,41 @@ double slice_sampler_1D(const gsl_rng *rng,
 
   /* Shrink interval to lower and upper bounds. */
 
-  if (L < lower) {
+  if ( L < lower ) {
     L = lower;
   }
-  if (R > upper) {
+  if ( R > upper ) {
     R = upper;
   }
 
   /* Sample from the interval, shrinking it on each rejection. */
 
-  double x1, gx1;
-  while (1) {
-    double W = R-L;
-    x1 = gsl_rng_uniform(rng)*W + L;
+  double x1 = x0, gx1 = _gx0;
+  register unsigned int niter = 0;
+  
+  do {
+    double W = R - L;
+    if ( W < _SS_ZERO_ ) break;
+    x1 = gsl_rng_uniform( rng ) * W + L;
 
     ++uni_slice_evals;
-    gx1 = (*g)(x1, gdata);
+    gx1 = ( *g )( x1, gdata );
+    if ( !gsl_finite( gx1 ) ) {
+      /* found undefined density value while sampling from interval */
+      x1 = x0;
+      gx1 = _gx0;
+      break;
+    }
 
-    if (gx1 >= logy) break;
+    if ( gx1 >= logy ) break;
 
-    if (x1 > x0) { R = x1; }
-    else         { L = x1; }
-  }
+    if ( x1 > x0 ) { R = x1; }
+    else           { L = x1; }
+  } while ( ++niter < _SS_MAXITER_ );
 
   /* Return the point sampled, with its log density attached */
 
-  if (gx0) *gx0 = gx1;
+  if ( gx0 ) *gx0 = gx1;
   return x1;
 }
 
@@ -224,12 +279,23 @@ int test_mix_normal(gsl_rng * rng)
   return 0;
 }
 
-int main()
+int main( )
 {
-  gsl_rng * rng = gsl_rng_alloc (gsl_rng_default);
+  gsl_rng * rng = gsl_rng_alloc( gsl_rng_default );
 
-  test_mix_normal(rng);
+  test_mix_normal( rng );
   return 0;
 }
 
 #endif
+
+/*
+  TODO:
+
+  - Comparar con gsl_fcmp
+
+  - Verificar que cada evaluacion de la densidad de un valor
+    finito. Decidir que hacer cuando de un valor no finito (infinito o
+    NAN)
+  
+ */
