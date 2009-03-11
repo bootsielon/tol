1941f9cad9e685c61e3e75db3aa2496dd82ff448
Víctor de Buen <vdebuen@bayesforecast.com>
2009-03-11 15:53:35 +0000

    Updating process to new web
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@728 be4846ae-f051-483a-831f-b64e7d08527f

--- windows/NSIS/send2web.bat
+++ windows/NSIS/send2web.bat
@@ -1,11 +1,15 @@
-@echo Creating automatic tag
-..\ActiveTOL\bin\tol -v create_tag_build.tol
-call create_tag_build.bat
+rem @echo Creating automatic tag
+rem ..\ActiveTOL\bin\tol -v create_tag_build.tol
+rem call create_tag_build.bat
+
+@echo Creating NSIS SETUP file
+..\ActiveTOL\bin\tol -v Tolbase.nsi.generate.tol
+makensis /V2 /X"SetCompressor /FINAL /SOLID lzma" ..\NSIS\Tolbase.nsi
+
 @echo Updating web release information
 ..\ActiveTOL\bin\tol -v -c"Text WriteFile(\"last_development_tol_release_win.txt\",Version);"
-pscp last_development_tol_release_win.txt tdt@cvs.tol-project.org:/home/bayes/pub/bin/win/last_development_tol_release_win.txt
+ pscp last_development_tol_release_win.txt toldevel@tolp.localbayes.es:/var/www/packages/win32/.
+
 @echo Updating web TOLBase installer
-pscp tolbase-1.1.7-setup.exe tdt@cvs.tol-project.org:/home/bayes/pub/bin/win/tolbase-cvstrunk-setup.exe
-@echo Moviendo el empaquetado a la carpeta de almacenamiento historico
-..\ActiveTOL\bin\tol -v store_history.tol
+ pscp Tolbase*.exe toldevel@tolp.localbayes.es:/var/www/packages/win32/.
 pause
