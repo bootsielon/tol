27dcd369a0403a9e499e31f23e3e48d62c69f818
Víctor de Buen <vdebuen@bayesforecast.com>
2009-04-04 18:25:09 +0000

    Adding BSR tests
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@888 be4846ae-f051-483a-831f-b64e7d08527f

--- tol_tests/run_all_tests.bat
+++ tol_tests/run_all_tests.bat
@@ -1,3 +1,4 @@
+..\windows\ActiveTol\bin\tol -v tol/_bsrTester.tol
 ..\windows\ActiveTol\bin\tol -v tol/_tolTester.tol
-..\windows\ActiveTol\bin\tol -v tol/_tolTester_knownbugs.tol
 ..\windows\ActiveTol\bin\tol -v toltcl/_toltclTester.tol
+..\windows\ActiveTol\bin\tol -v tol/_tolTester_knownbugs.tol
