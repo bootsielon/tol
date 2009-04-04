6a252a64f18e1db30b41bd6a40d4ccf2b97ecdf5
Víctor de Buen <vdebuen@bayesforecast.com>
2009-04-04 18:25:50 +0000

    Adding BSR tests
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@889 be4846ae-f051-483a-831f-b64e7d08527f

--- tol_tests/run_all_tests.sh
+++ tol_tests/run_all_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #\
+/usr/local/tol-gcc-release/bin/tolcon -v tol/_bsrTester.tol
 /usr/local/tol-gcc-release/bin/tolcon -v tol/_tolTester.tol
 /usr/local/tol-gcc-release/bin/tolcon -v tol/_tolTester_knownbugs.tol
 /usr/local/tol-gcc-release/bin/tolcon -v toltcl/_toltclTester.tol
