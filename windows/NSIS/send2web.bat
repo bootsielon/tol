d2afcd2e966d878232348feeb852a1b885a021fd
Víctor de Buen <vdebuen@bayesforecast.com>
2009-10-21 14:32:34 +0000

    Adding send2web.stdlib.bat
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@1546 be4846ae-f051-483a-831f-b64e7d08527f

--- windows/NSIS/send2web.bat
+++ windows/NSIS/send2web.bat
@@ -5,10 +5,10 @@ makensis /V2 /X"SetCompressor /FINAL /SOLID lzma" ..\NSIS\Tolbase.nsi
 @echo Updating web TOLBase installer
 pscp Tolbase*.exe toldevel@tolp.localbayes.es:/var/www/packages/win32/.
 
-call send2web.stdlib.bat
-
 @echo Updating web release information
 ..\ActiveTOL\bin\tol -v -c"Text WriteFile(\"development_tol_release_win.txt\",Version);"
 pscp development_tol_release_win.txt toldevel@tolp.localbayes.es:/var/www/packages/win32/.
 
-pause
+call send2web.stdlib.bat
+
+
---
+++ windows/NSIS/send2web.stdlib.bat
@@ -0,0 +1,4 @@
+@echo Compressing StdLib
+"c:\Archivos de programa\7-Zip\7z.exe" a -r -tzip StdLib.zip ..\ActiveTol\bin\StdLib
+@echo Updating web StdLib
+pscp StdLib.zip toldevel@tolp.localbayes.es:/var/www/packages/win32/development/.
