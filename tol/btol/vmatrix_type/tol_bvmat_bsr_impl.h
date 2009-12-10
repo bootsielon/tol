0759c4714dfb2f84bc0b7da4e7531cc860dcacaa
Víctor de Buen <vdebuen@bayesforecast.com>
2009-12-10 17:16:42 +0000

    Fixes #798
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@1780 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/btol/vmatrix_type/tol_bvmat_bsr_impl.h
+++ tol/btol/vmatrix_type/tol_bvmat_bsr_impl.h
@@ -720,6 +720,11 @@ public:
     noise_inf->sigmaName = "";
     noise_inf->sigmaIdx = -1;
     noise_inf->sigmaCte = d;
+    if(d==0)
+    {
+      Error(BSR()+"Cannot define regression segment "+noise_info_->name.c_str()+
+            " with variance cero");
+    }
     noise_inf->sigPriExpr = "";
     noise_inf->cov.Eye(noise_inf->size,noise_inf->size,0,d);
     noise_inf->L.Eye(noise_inf->size,noise_inf->size,0,sqrt(d));
