0bd9e1546c6a2d0ed0bfbfe9b9df4a8faf3e9004
Jorge Pérez <josp.jorge@gmail.com>
2009-12-22 10:32:57 +0000

    refs #823 : updated function descriptions, changing result in ANN.KDTree.KSearch now all neighbors are returned on the same matrix indices the same for distance info which is returned in dists.
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@1867 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/contrib/ANN/tolANN.cpp
+++ tol/contrib/ANN/tolANN.cpp
@@ -93,13 +93,13 @@ DefExtOpr(1, BDatANNKDTreeNew, "ANN.KDTree.New", 1, 1, "Matrix",
 	  I2("Build a kd-tree and return its identifier. A kd-tree is a "
              "complex data structure useful to perform nearest neighbourhood "
              "searches. When the kd-tree is not needed any more it should be "
-             "relased with ANN.KDTree.Delete\n"
+             "relased with ANN.KDTree.Delete\n\n"
              "See also ANN.KDTree.Delete, ANN.KDTree.RSearch and "
              "ANN.KDTree.KSearch",
              "Construye un kd-tree y retorna su identificador. Un kd-tree es "
              "una estructura de datos compleja util en la busqueda de "
              "vecinos cercanos. Cuando un kd-tree ya no se necesite mas debe "
-             "liberarse con ANN.KDTree.Delete\n"
+             "liberarse con ANN.KDTree.Delete\n\n"
              "Vease tambien ANN.KDTree.Delete, ANN.KDTree.RSearch y "
              "ANN.KDTree.KSearch"),
 	  BOperClassify::MatrixAlgebra_);
@@ -126,11 +126,11 @@ DeclareContensClass(BDat, BDatTemporary, BDatANNKDTreeDelete);
 DefIntOpr(1, BDatANNKDTreeDelete, "ANN.KDTree.Delete", 1, 1,
 	  "(Real kdtree)",
 	  I2("Release a kd-tree previously created with ANN.KDTree.New. "
-             "Returns 1 if success or 0 in case of fail.\n"
+             "Returns 1 if success or 0 in case of fail.\n\n"
              "See also ANN.KDTree.New, ANN.KDTree.RSearch and "
              "ANN.KDTree.KSearch",
              "Libera un kd-tree previamente creado con ANN.KDTree.New. "
-             "Retorna 1 se libero exitosamente o 0 en caso de fallo.\n"
+             "Retorna 1 si se libero exitosamente o 0 en caso de fallo.\n\n"
              "Vease tambien ANN.KDTree.New, ANN.KDTree.RSearch y "
              "ANN.KDTree.KSearch"),
 	  BOperClassify::MatrixAlgebra_);
@@ -151,16 +151,27 @@ DeclareContensClass(BSet, BSetTemporary, BSetANNKDTreeKSearch);
 DefExtOpr(1, BSetANNKDTreeKSearch, "ANN.KDTree.KSearch", 3, 3,
           "Real Matrix Real",
           "(Real kdtree, Matrix qpoints, Real k)",
-	  I2("Computes for every point in points the k nearest neighbors. "
-             "The result is a Set containing the matrix result for each "
-             "point. Each matrix result contain in the column 1 the index "
-             "the neighbors and in the column 2 the squared distance to the "
-             "query point. The index is referred to the ordering of the "
-             "points given in ANN.KDTree.New. The argument kdtree must be a "
-             "valid kdtree as returned from ANN.KDTree.New\n"
+	  I2("Computes for every point in qpoints the k nearest neighbors. "
+             "The result is a Set containing two matrix: indices and dists."
+             "The matrix indices contains for each point the set of neighbors "
+             "in increasing order of distance. The matrix dists contains the "
+             "corresponding squared distances.\n"
+             "The indices returned are referred to the ordering of the points "
+             "given in ANN.KDTree.New. The argument kdtree must be a valid "
+             "kd-tree as returned from ANN.KDTree.New\n\n"
              "See also ANN.KDTree.New, ANN.KDTree.Delete and "
              "ANN.KDTree.RSearch",
-             ""),
+             "Calcula para cada punto en qpoints los k vecinos mas cercanos. "
+             "El resultado es un conjunto con dos matrices: indices y dists."
+             "La matriz indices contiene para cada punto el conjunto de "
+             "vecinos en orden creciente de las distancias. La matriz dists "
+             "contiene las correspondientes distancias al cuadrado.\n"
+             "Los indices que se retornan son referidos al orden de los "
+             "puntos dados en la llamada a ANN.KDTree.New. El argumento "
+             "kdtree debe ser un kd-tree valido retornado desde "
+             "ANN.KDTree.New\n\n"
+             "Vease tambien ANN.KDTree.New, ANN.KDTree.Delete y "
+             "ANN.KDTree.RSearch"),
 	  BOperClassify::MatrixAlgebra_);
 //--------------------------------------------------
 void BSetANNKDTreeKSearch::CalcContens()
@@ -168,8 +179,6 @@ void BSetANNKDTreeKSearch::CalcContens()
   double addr = Dat( Arg( 1 ) ).Value();
   tol_kdtree *tree = tol_kdtree::decode_addr( addr );
   DMat &qpoints = dMat( Arg( 2 ) );
-  BList* lst_result = NIL;
-  BList* lst_aux = NIL;
   
   BDat dK = Dat( Arg( 3 ) );
 
@@ -181,32 +190,37 @@ void BSetANNKDTreeKSearch::CalcContens()
   int k = int( round( dK.Value( ) ) );
   ANNpoint pt = annAllocPt( tree->get_dim(), 0.0 );
   ANNidxArray nn_idx = new ANNidx[k];
-  ANNdistArray dists = new ANNdist[k];
-  double *buffer = ((DMat&)qpoints).GetData().GetBuffer();
-  BMat nn_mat; nn_mat.Alloc( k, 2 );
+  ANNdistArray nn_dists = new ANNdist[k];
+  double *qp_buffer = ((DMat&)qpoints).GetData().GetBuffer();
+  BMat nn_mat; nn_mat.Alloc( qpoints.Rows( ), k );
   double *nn_buffer = ((DMat&)nn_mat).GetData().GetBuffer();
+  BMat dists_mat; dists_mat.Alloc( qpoints.Rows( ), k );
+  double *dists_buffer = ((DMat&)dists_mat).GetData().GetBuffer();
   for ( int r = 0; r < qpoints.Rows( ); r++ ) {
     for ( int c = 0; c < qpoints.Columns( ); c++ ) {
-      pt[ c ] = *buffer++;
+      pt[ c ] = *qp_buffer++;
     }
     tree->get_kdtree()->annkSearch( pt,      // query point
                                     k,       // number of near neighbors
                                     nn_idx,  // nearest neighbors (returned)
-                                    dists,   // distance (returned)
+                                    nn_dists,   // distance (returned)
                                     0.0 );
-    // result matrix for pt
-    double* ptr = nn_buffer;
-    for( int _r = 0; _r < k; ++_r ) {
-      *ptr++ = double( nn_idx[ _r ] );
-      *ptr++ = dists[ _r ];
+    // results rows for pt
+    for( int _k = 0; _k < k; ++_k ) {
+      *nn_buffer++ = double( nn_idx[ _k ] );
+      *dists_buffer++ = nn_dists[ _k ];
     }
-    BUserMat* nn_umat = BContensMat::New( "", nn_mat, "" );
-    LstFastAppend( lst_result, lst_aux, nn_umat );
   }
   assert( (buffer - ((DMat&)qpoints).GetData().GetBuffer()) ==
           qpoints.Rows()*qpoints.Columns() );
   annDeallocPt( pt );
   delete [] nn_idx;
-  delete [] dists;
+  delete [] nn_dists;
+  BList* lst_result = NIL;
+  BUserMat* nn_umat = BContensMat::New( "", nn_mat, "" );
+  nn_umat->PutName( "indices" );
+  BUserMat* dists_umat = BContensMat::New( "", dists_mat, "" );
+  dists_umat->PutName( "dists" );
+  lst_result = Cons( nn_umat, Cons( dists_umat, NIL ) );
   contens_.RobElement( lst_result );
 }
