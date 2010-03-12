13bedb153b3a7468a2455d6eeafb346c87bc86ac
Jorge Pérez <josp.jorge@gmail.com>
2010-03-12 12:45:02 +0000

    updating build in Linux
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@2054 be4846ae-f051-483a-831f-b64e7d08527f

--- tolsh/build.sh
+++ tolsh/build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+tclkit=tclkitsh-8.5.8
 version=2.0.1
 sys=`uname`
 case $sys in
 Linux)
   echo Building for Linux ...
-  cp Linux/tclkit-linux-x86.upx.bin Linux/tclkit
-  Linux/tclkit sdx.kit wrap tolsh -runtime Linux/tclkit-linux-x86.upx.bin
+  cp Linux/$tclkit Linux/tclkit
+  Linux/tclkit sdx.kit wrap tolsh -runtime Linux/$tclkit
   mv tolsh Linux/tolsh$version
   rm Linux/tclkit
   echo tolsh$version was generated on Linux/
