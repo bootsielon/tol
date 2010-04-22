6ad2ca7b533589e7e6996eaa59f93fbbb5fb6829
Víctor de Buen <vdebuen@bayesforecast.com>
2010-04-22 14:19:37 +0000

    setting VERSION_BUILD   "0.37.alpha"
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@2115 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/tol_version_def.h
+++ tol/tol_version_def.h
@@ -24,7 +24,7 @@
 #define VERSION_MAJOR   2
 #define VERSION_MINOR   0
 #define VERSION_RELEASE 1
-#define VERSION_BUILD   "0.36.alpha"
+#define VERSION_BUILD   "0.37.alpha"
 
 #if defined(_MSC_VER)
 #include <tol_build_date.h>
