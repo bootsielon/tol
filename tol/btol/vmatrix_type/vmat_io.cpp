27bba6820b8ef88a1bd08b29544e71dd761d4bcf
Jorge Pérez <josp.jorge@gmail.com>
2010-05-31 10:32:36 +0000

    some syntax errors reached under #ifdef _USE_MM_IO_
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@2201 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/btol/vmatrix_type/vmat_io.cpp
+++ tol/btol/vmatrix_type/vmat_io.cpp
@@ -341,9 +341,9 @@ void BVMat::cholmod_error_handler(int status,char *file,int line,char *message)
   MM_typecode matcode;
   
   if (code_==ESC_chlmRsparse) {
-    cholmod_R_sparse unsym = cholmod_copy(s_.chlmRsparse_, 0, 1, common_);
+    cholmod_R_sparse *unsym = cholmod_copy(s_.chlmRsparse_, 0, 1, common_);
     ptr_triplet = cholmod_sparse_to_triplet(unsym, common_);
-    cholmod_free_sparse(unsym); 
+    cholmod_free_sparse(&unsym, common_); 
     should_free = true;
   } else if (code_==ESC_chlmRtriplet) {
     ptr_triplet = s_.chlmRtriplet_;
