73bfcd15e3d4f4e0c7d903ba124f0a9d7e4dc0ca
Víctor de Buen <vdebuen@bayesforecast.com>
2010-07-27 17:38:20 +0000

    Refs #947 Im sorry, Code members must not assign the NameBlock to its operator
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@2412 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/btol/bgrammar/nameblock.cpp
+++ tol/btol/bgrammar/nameblock.cpp
@@ -660,11 +660,16 @@ const BText& BNameBlock::LocalName() const
     return(false);  
   }
   obj->PutNameBlock(this);
+/*
   if((obj->Grammar()==GraCode())&&(obj->Mode()==BOBJECTMODE))
   {
     BOperator* opr = Code(obj).Operator();
-    opr->PutNameBlock(this);
+    if(!(opr->NameBlock()))
+    {
+      opr->PutNameBlock(this);
+    }
   }
+*/
   if(name[0]!='_')
   {
     nonPrivateMembers_++;
