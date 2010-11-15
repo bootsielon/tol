dc12c4a1dbd04d1e5f16a39a5f830fc47e10ce78
Víctor de Buen <vdebuen@bayesforecast.com>
2010-11-15 16:15:36 +0000

    separating processes of building and uploading
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@2677 be4846ae-f051-483a-831f-b64e7d08527f

--- windows/NSIS/send2web.bat
+++ windows/NSIS/send2web.bat
@@ -1,14 +1,12 @@
-@echo Creating NSIS SETUP file
-..\ActiveTOL\bin\tol -v Tolbase.nsi.generate.tol
-makensis /V2 /X"SetCompressor /FINAL /SOLID lzma" ..\NSIS\Tolbase.nsi
 
 @echo Updating web TOLBase installer
-pscp Tolbase*.exe toldevel@tolp.localbayes.es:/var/www/packages/win32/.
+pscp tolbase-v2.0.1-setup.exe toldevel@tolp.localbayes.es:/var/www/packages/win32/.
 
 @echo Updating web release information
 ..\ActiveTOL\bin\tol -v -c"Text WriteFile(\"development_tol_release_win.txt\",Version);"
 pscp development_tol_release_win.txt toldevel@tolp.localbayes.es:/var/www/packages/win32/.
 
+pause
 call send2web.stdlib.bat
 
 
