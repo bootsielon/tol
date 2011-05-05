008300b93b8a15e61d884c0fd489e545c88083bc
Víctor de Buen <vdebuen@bayesforecast.com>
2011-05-05 12:54:41 +0000

    Refs #1196
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@3504 be4846ae-f051-483a-831f-b64e7d08527f

--- windows/NSIS/send2web.bat
+++ windows/NSIS/send2web.bat
@@ -1,10 +1,5 @@
-
-@echo Updating web TOLBase installer
-pscp tolbase-v2.0.1-setup.exe toldevel@tolp.localbayes.es:/var/www/packages/win32/.
-
 @echo Updating web release information
-..\ActiveTOL\bin\tol -v -c"Text WriteFile(\"official_tol_release_win.txt\",Version);"
-pscp official_tol_release_win.txt toldevel@tolp.localbayes.es:/var/www/packages/win32/.
+..\ActiveTOL\bin\tol -v send2web.tol
 
 pause
 call send2web.stdlib.bat
