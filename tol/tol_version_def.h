4c13d5ca19d5152ffad0194bb329bb4bbf7f51a1
Víctor de Buen <vdebuen@bayesforecast.com>
2012-02-13 20:27:39 +0000

    VERSION_BUILD   "b026"
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@4129 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/tol_version_def.h
+++ tol/tol_version_def.h
@@ -23,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 #define VERSION_MAJOR   3
 #define VERSION_MINOR   1
-#define VERSION_BUILD   "b025"
+#define VERSION_BUILD   "b026"
 
 #if defined(_MSC_VER)
 #include "tol_build_date.h"
