63ef2284c48cc0e434b75ca190a003b6af790e7a
Jorge Pérez <josp.jorge@gmail.com>
2012-06-01 07:54:14 +0000

    next patch will be p011
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@4757 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/tol_version_def.h
+++ tol/tol_version_def.h
@@ -23,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 #define VERSION_MAJOR   3
 #define VERSION_MINOR   2
-#define VERSION_BUILD   "b010"
+#define VERSION_BUILD   "b011"
 
 #if defined(_MSC_VER)
 #include "tol_build_date.h"
