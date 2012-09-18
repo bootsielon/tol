de399ff3402f0ac06c9094d0f4cf320309c0f519
Jorge Pérez <josp.jorge@gmail.com>
2012-09-18 10:45:05 +0000

    refs #1598, #1599 preparing next TOL versión that will solve tickets 1598 & 1599
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@5071 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/tol_version_def.h
+++ tol/tol_version_def.h
@@ -23,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 #define VERSION_MAJOR   3
 #define VERSION_MINOR   2
-#define VERSION_BUILD   "b013"
+#define VERSION_BUILD   "b014"
 
 #if defined(_MSC_VER)
 #include "tol_build_date.h"
