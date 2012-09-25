d404d8572fbdee492d724b1d45822205c08e2998
Víctor de Buen <vdebuen@bayesforecast.com>
2012-09-25 07:14:10 +0000

    Setting VERSION_BUILD   "b015"
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@5129 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/tol_version_def.h
+++ tol/tol_version_def.h
@@ -23,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////////
 #define VERSION_MAJOR   3
 #define VERSION_MINOR   2
-#define VERSION_BUILD   "b014"
+#define VERSION_BUILD   "b015"
 
 #if defined(_MSC_VER)
 #include "tol_build_date.h"
