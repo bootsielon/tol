e15d2f70702e4f3ca791c5d520e9d164d8595834
Pedro Gea <pgea@bayesforecast.com>
2013-07-18 12:53:30 +0000

    Refs #1699 A raíz del ''warning'' de compilación: {{{ 9>..\btol\timeset_type\tms.cpp(923) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(930) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(944) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(953) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(964) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(994) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(1019) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(1027) : warning C4305: 'argument' : truncation from 'int' to 'bool' 9>..\btol\timeset_type\tms.cpp(1130) : warning C4305: 'argument' : truncation from 'int' to 'bool' }}}
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@5951 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/btol/timeset_type/tol_btmset.h
+++ tol/btol/timeset_type/tol_btmset.h
@@ -118,7 +118,7 @@ public:
   virtual const BText& TestAborted(const BText& cod, 
                                    const BText& msg, 
                                    const BText& interval);
-  virtual bool AbortMessageSent(bool sign) { return(false); }
+  virtual bool AbortMessageSent(short sign) { return(false); }
 
   BBool IsCompatibleWith   (const BTimeSet& tms,
 	                          BDate first = BDate::DefaultFirst(),
