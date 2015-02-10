59889f34dc6af48d120deccb9ffed0ee51a7240b
Jorge Pérez <josp.jorge@gmail.com>
2015-02-10 08:27:23 +0000

    bmath/bprdist/prd.cpp: In member function ‘virtual BDat BProbDist::Inverse(BDat, BDat)’: bmath/bprdist/prd.cpp:258:18: warning: ‘n3’ may be used uninitialized in this function [-Wmaybe-uninitialized]      BInt n1, n2, n3, n4, n5;                   ^ bmath/bprdist/prd.cpp:276:38: warning: ‘n5’ may be used uninitialized in this function [-Wmaybe-uninitialized]      if((n1<0)||(n2<0)||(n3<0)||(n4<0)||(n5<0))                                       ^ bmath/bprdist/prd.cpp:276:35: warning: ‘n4’ may be used uninitialized in this function [-Wmaybe-uninitialized]      if((n1<0)||(n2<0)||(n3<0)||(n4<0)||(n5<0))                                    ^ bmath/bprdist/prd.cpp:276:19: warning: ‘n2’ may be used uninitialized in this function [-Wmaybe-uninitialized]      if((n1<0)||(n2<0)||(n3<0)||(n4<0)||(n5<0))
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@6453 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/bmath/bprdist/prd.cpp
+++ tol/bmath/bprdist/prd.cpp
@@ -255,7 +255,7 @@ BDat BProbDist::Inverse(BDat prob, BDat tolerance)
     if(prob==0) { return(min_); }
     if(prob==1) { return(max_); }
     BDat	x  = InverseAprox(prob,tolerance);
-    BInt	n1, n2, n3, n4, n5;
+    BInt	n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0;
     n1 = InverseFrom(x, prob, tolerance);
     if(n1==0)
     {
