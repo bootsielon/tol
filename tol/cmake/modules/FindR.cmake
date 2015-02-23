5dd248892579a323353bd84cb1ff61e5908d8989
Jorge Pérez <josp.jorge@gmail.com>
2015-02-23 12:52:49 +0000

    refs #1818, #1812, mejoras en la busqueda de R en Windows
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@6494 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/cmake/modules/FindR.cmake
+++ tol/cmake/modules/FindR.cmake
@@ -4,6 +4,7 @@
 #
 # Defines the following:
 #  R_COMMAND           - Path to R command
+#  Rscript_COMMAND     - Path to Rscript command
 #  R_HOME              - Path to 'R home', as reported by R
 #  R_INCLUDE_DIR       - Path to R include directory
 #  R_LIBRARY_BASE      - Path to R library
@@ -27,6 +28,7 @@
 set(TEMP_CMAKE_FIND_APPBUNDLE ${CMAKE_FIND_APPBUNDLE})
 set(CMAKE_FIND_APPBUNDLE "NEVER")
 find_program(R_COMMAND R DOC "R executable.")
+find_program(Rscript_COMMAND Rscript DOC "Rscript executable.")
 set(CMAKE_FIND_APPBUNDLE ${TEMP_CMAKE_FIND_APPBUNDLE})
 
 if(R_COMMAND)
@@ -45,26 +47,64 @@ if(R_COMMAND)
   # the following command does nothing currently, but will be used when deprecated code is removed
   set(R_HOME ${R_ROOT_DIR} CACHE PATH "R home directory obtained from R RHOME")
 
-  find_path(R_INCLUDE_DIR R.h
-            HINTS ${R_ROOT_DIR}
-            PATHS /usr/local/lib /usr/local/lib64 /usr/share
-            PATH_SUFFIXES include R/include
-            DOC "Path to file R.h")
+  if( WIN32 )
+    find_path( R_INCLUDE_DIR R.h
+      PATHS ${R_ROOT_DIR}
+      PATH_SUFFIXES include R/include
+      DOC "Path to file R.h"
+      NO_DEFAULT_PATH )
 
-  find_library(R_LIBRARY_BASE R
-            HINTS ${R_ROOT_DIR}/lib
-            DOC "R library (example libR.a, libR.dylib, etc.).")
+    if( CMAKE_SIZEOF_VOID_P EQUAL 4 )
+      set( path_suffix "i386" )
+    else( CMAKE_SIZEOF_VOID_P EQUAL 4 )
+      set( path_suffix "x64" )
+    endif( CMAKE_SIZEOF_VOID_P EQUAL 4 )
 
-  find_library(R_LIBRARY_BLAS NAMES Rblas blas
-            HINTS ${R_ROOT_DIR}/lib
-            DOC "Rblas library (example libRblas.a, libRblas.dylib, etc.).")
+    find_library(R_LIBRARY_BASE R
+      PATHS ${R_ROOT_DIR}/bin
+      PATH_SUFFIXES ${path_suffix}
+      DOC "R library (example libR.a, libR.dylib, etc.)."
+      NO_DEFAULT_PATH )
 
-  find_library(R_LIBRARY_LAPACK NAMES Rlapack lapack
-            HINTS ${R_ROOT_DIR}/lib
-            DOC "Rlapack library (example libRlapack.a, libRlapack.dylib, etc.).")
+    find_library(R_LIBRARY_BLAS NAMES Rblas blas
+      PATHS ${R_ROOT_DIR}/bin
+      PATH_SUFFIXES ${path_suffix}
+      DOC "Rblas library (example libRblas.a, libRblas.dylib, etc.)."
+      NO_DEFAULT_PATH )
+
+    find_library(R_LIBRARY_LAPACK NAMES Rlapack lapack
+      PATHS ${R_ROOT_DIR}/bin
+      PATH_SUFFIXES ${path_suffix}
+      DOC "Rlapack library (example libRlapack.a, libRlapack.dylib, etc.)."
+      NO_DEFAULT_PATH )
+  else( WIN32 )
+    execute_process( COMMAND ${R_COMMAND} CMD config --cppflags
+      OUTPUT_VARIABLE R_CPPFLAGS OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if ( ${R_CPPFLAGS} MATCHES "[-][I]([^ ;])+" )
+      string( SUBSTRING ${CMAKE_MATCH_0} 2 -1 _INCL )
+      set( R_INCL_DIR ${_INCL} )
+      message( STATUS "R_INCL_DIR = ${RCPP_INCL_DIR}" )
+    endif( )
+
+    find_path( R_INCLUDE_DIR R.h
+      PATHS ${R_INCL_DIR}
+      DOC "Path to file R.h" )
+
+    find_library(R_LIBRARY_BASE R
+      HINTS ${R_ROOT_DIR}/lib
+      DOC "R library (example libR.a, libR.dylib, etc.).")
+
+    find_library(R_LIBRARY_BLAS NAMES Rblas blas
+      HINTS ${R_ROOT_DIR}/lib
+      DOC "Rblas library (example libRblas.a, libRblas.dylib, etc.).")
+
+    find_library(R_LIBRARY_LAPACK NAMES Rlapack lapack
+      HINTS ${R_ROOT_DIR}/lib
+      DOC "Rlapack library (example libRlapack.a, libRlapack.dylib, etc.).")
+  endif( WIN32 )
 
   find_library(R_LIBRARY_READLINE readline
-            DOC "(Optional) system readline library. Only required if the R libraries were built with readline support.")
+    DOC "(Optional) system readline library. Only required if the R libraries were built with readline support.")
 
 else(R_COMMAND)
   message(SEND_ERROR "FindR.cmake requires the following variables to be set: R_COMMAND")
