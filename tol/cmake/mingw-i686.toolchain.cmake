0e6d96d33fd711fb8545d4845ec7ab96f54a44b8
Jorge Pérez <josp.jorge@gmail.com>
2015-05-06 08:57:31 +0000

    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@6652 be4846ae-f051-483a-831f-b64e7d08527f

--- tol/cmake/mingw-i686.toolchain.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_SYSTEM_NAME Windows)
-
-set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32 -mno-align-double" CACHE STRING "c++ flags" )
-set( CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32 -mno-align-double" CACHE STRING "c flags" )
-set( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -ggdb" CACHE STRING "c++ flags in Debug" )
-set( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -O0 -ggdb" CACHE STRING "c flags in Debug" )
-set( CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -O2" CACHE STRING "c++ flags in Release" )
-set( CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -O2" CACHE STRING "c flags in Release" )
-set( LINKER_SHARED_FLAGS "${LINKER_SHARED_FLAGS} -m32" CACHE STRING "linker flags" )
