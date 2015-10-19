b567d4cb4fe16ab4a730f4745e38d8369800fa5d
Pedro Gea <pgea@bayesforecast.com>
2015-10-19 10:46:35 +0000

    Refs #1880 Scripts de compilación de paquetes para Linux32 Otros cambios
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@6956 be4846ae-f051-483a-831f-b64e7d08527f

--- building/Linux_32/build_toltcl_release.sh
+++ building/Linux_32/build_toltcl_release.sh
@@ -5,8 +5,7 @@ CMAKE_DIR=BUILD_CMAKE/m32/release
 
 echo -n "mkdir? (y/n) "
 read answer
-if [ "$answer" = "y" ]
-then
+if [ "$answer" = "y" ]; then
   if [ -d "$CMAKE_DIR" ]; then 
     rm -r "$CMAKE_DIR"
   fi
@@ -17,22 +16,19 @@ cd $CMAKE_DIR
 
 echo -n "cmake? (y/n) "
 read answer
-if [ "$answer" = "y" ]
-then
+if [ "$answer" = "y" ]; then
   cmake ../../.. -DCMAKE_BUILD_TYPE=Release -DTOL_PREFIX_PATH=/usr/local/tol3.2-release -DCMAKE_INSTALL_PREFIX=/usr/local/tol3.2-release
 fi
 
 echo -n "make? (y/n) "
 read answer
-if [ "$answer" = "y" ]
-then
+if [ "$answer" = "y" ]; then
   make
 fi
 
 echo -n "install? (y/n) "
 read answer
-if [ "$answer" = "y" ]
-then
+if [ "$answer" = "y" ]; then
   sudo make install
 fi
 
