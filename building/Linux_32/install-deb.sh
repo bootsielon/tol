30f6735774b068c0b9b94df2dc4fe6dd5f757228
Pedro Gea <pgea@bayesforecast.com>
2015-11-16 09:45:57 +0000

    Refs #1860 Se modifican las dependencias para DEB. Se incorporan algunas líneas (comentadas) sobre la creación de scripts para el lanzamiento de tolcon y tolsh, para revisar. Se incorporan líneas para la instalación/desinstalación de dependencias en install-deb.sh
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@7038 be4846ae-f051-483a-831f-b64e7d08527f

--- building/Linux_32/install-deb.sh
+++ building/Linux_32/install-deb.sh
@@ -7,11 +7,25 @@ then
   sudo dpkg -i installer/tol-3.2.deb
 fi
 
+echo -n "install dependences? (y/n) "
+read answer
+if [ "$answer" = "y" ]
+then
+  sudo apt-get -f install
+fi
+
 echo -n "uninstall? (y/n) "
 read answer
 if [ "$answer" = "y" ]
 then
   sudo dpkg -r tol
+fi
+
+echo -n "uninstall dependences? (y/n) "
+read answer
+if [ "$answer" = "y" ]
+then
+  sudo apt-get autoremove
 fi
 
 echo -n "press enter to exit "
