a6c25590bbb57524b80aa7e1bc3bf7491bc38bca
Jorge Pérez <josp.jorge@gmail.com>
2015-11-20 13:23:17 +0000

    refs #1860, script de arranque de TOL basado en tolenv.sh
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@7053 be4846ae-f051-483a-831f-b64e7d08527f

---
+++ tol/tolcon.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Shell wrapper for TOL executable.
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+echo $SCRIPTPATH
+
+if [ -f ${SCRIPTPATH}/tolenv.sh ] ; then
+  source ${SCRIPTPATH}/tolenv.sh
+else
+  echo "${SCRIPTPATH}/tolenv.sh not found, running without environment"
+fi
+
+exec ${SCRIPTPATH}/tolcon "${@}"
