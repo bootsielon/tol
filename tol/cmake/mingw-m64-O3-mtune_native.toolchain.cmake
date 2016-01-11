a61660c8e75c7d12024108910829f4d1b7612f67
Pedro Gea <pgea@bayesforecast.com>
2016-01-11 10:01:08 +0000

    Refs #1907 La opción -O3 permite pasar estos cuatro tests.
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@7185 be4846ae-f051-483a-831f-b64e7d08527f

---
+++ tol/cmake/mingw-m64-O3-mtune_native.toolchain.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set( CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -m64 -mno-align-double"                 CACHE STRING "c++ flags" )
+set( CMAKE_C_FLAGS             "${CMAKE_C_FLAGS} -m64 -mno-align-double"                   CACHE STRING "c flags" )
+set( CMAKE_CXX_FLAGS_DEBUG     "${CMAKE_CXX_FLAGS_DEBUG} -m64 -O0 -ggdb -mno-align-double" CACHE STRING "c++ flags in Debug" )
+set( CMAKE_C_FLAGS_DEBUG       "${CMAKE_C_FLAGS_DEBUG} -m64 -O0 -ggdb -mno-align-double"   CACHE STRING "c flags in Debug" )
+set( CMAKE_CXX_FLAGS_RELEASE   "${CMAKE_CXX_FLAGS_RELEASE} -m64 -O3 -mtune=native -mno-align-double -DNDEBUG"     CACHE STRING "c++ flags in Release" )
+set( CMAKE_C_FLAGS_RELEASE     "${CMAKE_C_FLAGS_RELEASE} -m64 -O3 -mtune=native -mno-align-double -DNDEBUG"       CACHE STRING "c flags in Release" )
+set( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -m64"                         CACHE STRING "linker flags" )
