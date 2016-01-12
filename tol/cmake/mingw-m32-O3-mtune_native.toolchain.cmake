eb0481b0a68e48d3ebf86d208d54f9ccc7286d9b
Pedro Gea <pgea@bayesforecast.com>
2016-01-12 13:09:14 +0000

    Refs #1910 Se revisan las opciones de compilación y se incoporpora en CMAKE_SHARED_LINKER_FLAGS las necesarias para la librería de vbtol. Véase: http://www.mingw.org/wiki/Visual_Basic_DLL
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@7189 be4846ae-f051-483a-831f-b64e7d08527f

---
+++ tol/cmake/mingw-m32-O3-mtune_native.toolchain.cmake
@@ -0,0 +1,9 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -m32 -mno-align-double"             CACHE STRING "c++ flags" )
+set(CMAKE_C_FLAGS             "${CMAKE_C_FLAGS} -m32 -mno-align-double"               CACHE STRING "c flags" )
+set(CMAKE_CXX_FLAGS_DEBUG     "${CMAKE_CXX_FLAGS_DEBUG} -O0 -ggdb"                    CACHE STRING "c++ flags in Debug" )
+set(CMAKE_C_FLAGS_DEBUG       "${CMAKE_C_FLAGS_DEBUG} -O0 -ggdb"                      CACHE STRING "c flags in Debug" )
+set(CMAKE_CXX_FLAGS_RELEASE   "${CMAKE_CXX_FLAGS_RELEASE} -DNDEBUG -O3 -mtune=native" CACHE STRING "c++ flags in Release" )
+set(CMAKE_C_FLAGS_RELEASE     "${CMAKE_C_FLAGS_RELEASE} -DNDEBUG -O3 -mtune=native"   CACHE STRING "c flags in Release" )
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -m32 -Wl,--add-stdcall-alias -Wl,--enable-stdcall-fixup" CACHE STRING "linker flags" )
