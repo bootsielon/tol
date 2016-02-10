db5a963a92aaee3b23bafb50ef38ebf6f65d6211
Pedro Gea <pgea@bayesforecast.com>
2016-02-10 11:18:09 +0000

    Refs #1899 Algunos cambios pendientes.
    git-svn-id: https://www.tol-project.org/svn/tolp/trunk@7215 be4846ae-f051-483a-831f-b64e7d08527f

--- tolbase/lib/wtree/wtree.tcl
+++ tolbase/lib/wtree/wtree.tcl
@@ -1,7 +1,9 @@
 if { [ string compare $::tcl_version "8.5" ] >= 0 } {
   package require tile
 }
-package require treectrl 2.2.9
+
+#(pgea) package require treectrl 2.2.9
+package require treectrl 2.2.8
 package require autoscroll
 package require snit
 
